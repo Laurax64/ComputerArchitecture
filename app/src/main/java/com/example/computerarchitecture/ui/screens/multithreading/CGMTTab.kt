8ca1ef55afaa1ff71bf3855badef8387c3bc9cce
laura.voetzsch <laura.voetzsch@logiball.de>
2024-05-09 17:32:40 +0200

    Remove visualisation
    Signed-off-by: laura.voetzsch <laura.voetzsch@logiball.de>

--- app/src/main/java/com/example/computerarchitecture/ui/screens/multithreading/CGMTTab.kt
+++ app/src/main/java/com/example/computerarchitecture/ui/screens/multithreading/CGMTTab.kt
@@ -1,29 +1,15 @@
 package com.example.computerarchitecture.ui.screens.multithreading
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.computerarchitecture.data.CAThread
-import com.example.computerarchitecture.data.Cache
-import com.example.computerarchitecture.data.ProcessingUnit
-import com.example.computerarchitecture.data.exampleCaches
-import com.example.computerarchitecture.data.exampleThreads
-import com.example.computerarchitecture.data.exampleUnits
 
 
 /**
@@ -43,92 +29,6 @@ fun CGMTTab(
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.spacedBy(12.dp),
     ) {
-        var threads = exampleThreads
-        val caches = exampleCaches
-        var units = exampleUnits
 
-
-        var resultThread: CAThread by remember {
-            mutableStateOf(
-                CAThread(
-                    0,
-                    "",
-                    0,
-                    mutableListOf()
-                )
-            )
-        }
-
-        Text("Hit under N Miss:", style = MaterialTheme.typography.titleMedium)
-        Text("1")
-        Text("Context Switch Overhead:", style = MaterialTheme.typography.titleMedium)
-        Text("1 cycle")
-        Text("Processing Units:", style = MaterialTheme.typography.titleMedium)
-        units.forEach {
-            Text("Unit ${it.id}, type ${it.type}, latency ${it.numberOfLatencyCycles}")
-        }
-        Text("Caches:", style = MaterialTheme.typography.titleMedium)
-        caches.forEach {
-            Text("${it.type}, hit latency ${it.hitLatency}, miss latency ${it.missLatency}")
-        }
-
-
-
-        DisplayThreads(threads = threads, units = units)
-
-        //  Execute the Coarse Grain Multithreading algorithm if the user has added an L2 cache
-        Button(
-            {
-                val l2Cache = caches.find { it.type == "L2 Cache" }
-                if (l2Cache == null) {
-                    Toast.makeText(
-                        null,
-                        "Please add an L2 cache to continue",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                } else {
-                    resultThread =
-                        coarseGrainMultiTreading(threads, units, caches, l2Cache.missLatency, 1, 1)
-                }
-            },
-            Modifier.fillMaxWidth()
-        ) {
-            Text("Execute CGMT")
-        }
-        DisplayThread(resultThread, units, calculateMaxEndTime(listOf(resultThread)))
     }
-}
-
-/**
- * Applies the Coarse Grain Multithreading algorithm to the given threads and units
- *
- * @param threads The threads to apply the algorithm to
- * @param units The processing units to use
- * @param caches The caches
- * @param l2CacheMiss The latency of an L2 cache miss
- * @param hitUnderNMiss The value n for hit under n miss
- * @param contextSwitchOverhead The context switch overhead
- * @return The threads after applying the algorithm
- */
-fun coarseGrainMultiTreading(
-    threads: List<CAThread>,  // Change parameter type to List to ensure immutability
-    units: List<ProcessingUnit>,
-    caches: List<Cache>,
-    l2CacheMiss: Int,
-    hitUnderNMiss: Int,
-    contextSwitchOverhead: Int
-): CAThread {
-    val resultThread = CAThread(0, "", 0, mutableListOf())
-    // Threads with operations sorted by unit id
-    val sortedThreads = threads.map { thread ->
-        val sortedOperations = thread.operations.sortedBy { it.unitId }.toMutableList()
-        CAThread(thread.id, thread.name, thread.priority, sortedOperations)
-    }.sortedBy { it.operations.first().unitId }
-
-    for (thread in sortedThreads) {
-        for (operation in thread.operations) {
-            resultThread.operations.add(operation)
-        }
-    }
-    return resultThread
 }
--- app/src/main/java/com/example/computerarchitecture/ui/screens/multithreading/FGMTTab.kt
+++ app/src/main/java/com/example/computerarchitecture/ui/screens/multithreading/FGMTTab.kt
@@ -6,22 +6,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.computerarchitecture.data.CAThread
-import com.example.computerarchitecture.data.Operation
-import com.example.computerarchitecture.data.ProcessingUnit
-import com.example.computerarchitecture.data.exampleThreads
-import com.example.computerarchitecture.data.exampleUnits
-import java.util.Collections.max
 
 /**
  * Displays the fine grain multithreading tab
@@ -32,8 +20,6 @@ import java.util.Collections.max
 fun FGMTTab(
     modifier: Modifier = Modifier,
 ) {
-    var configureNewOperation by remember { mutableStateOf(false) }
-
     Column(
         modifier
             .fillMaxWidth()
@@ -42,78 +28,7 @@ fun FGMTTab(
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.spacedBy(12.dp),
     ) {
-        val threads: MutableList<CAThread> = exampleThreads
-        val units: MutableList<ProcessingUnit> = exampleUnits
-        var resultThread: CAThread by remember {
-            mutableStateOf(
-                CAThread(
-                    0,
-                    "",
-                    0,
-                    mutableListOf()
-                )
-            )
-        }
 
-        // UI elements for configuring caches, processing units, and threads...
-
-        // Execute the Fine Grain Multithreading algorithm if the user has added an L2 cache
-        Button(
-            {
-                resultThread = fineGrainMultithreading(threads)
-            },
-            Modifier.fillMaxWidth()
-        ) {
-            Text("Execute FGMT")
-        }
-        DisplayThread(resultThread, units, calculateMaxEndTime(listOf(resultThread)))
     }
 }
-
-/**
- * Applies the Fine Grain Multithreading algorithm to the given threads and units
- *
- * @param threads The threads to apply the algorithm to
- * @return The threads after applying the algorithm
- */
-fun fineGrainMultithreading(threads: MutableList<CAThread>): CAThread {
-    val resultThread = CAThread(0, "", 0, mutableListOf())
-    val threadsCopy: MutableList<CAThread> = mutableListOf()
-    threadsCopy.addAll(threads)
-    // Sort the threads by the number
-    var start = 0
-    var end = 1
-    val operations = mutableListOf<Operation>()
-
-    var maxOperationCount = max(threadsCopy.map { it.operations.size })
-    var unitIndex = 0
-    while (unitIndex < 4) {
-        var operationIndex = 0
-        while (operationIndex < maxOperationCount) {
-            var threadIndex = 0
-            while (threadIndex < threadsCopy.size) {
-                if (threads[threadIndex].operations.size > operationIndex) {
-                    if (threads[threadIndex].operations[operationIndex].unitId == unitIndex) {
-                        operations.add(
-                            Operation(
-                                threads[threadIndex].operations[operationIndex].unitId,
-                                threads[threadIndex].operations[operationIndex].threadId,
-                                start,
-                                end
-                            )
-                        )
-                    }
-                    operationIndex++
-                }
-                threadIndex++
-            }
-        }
-        start++
-        end++
-        unitIndex++
-    }
-    resultThread.operations.addAll(operations.sortedBy { it.start })
-    return resultThread
-}
-
 
