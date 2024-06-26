81494cd4fd60beafa19704ceda2ac09547222e9b
Laura Voetzsch <56238822+laurax64@users.noreply.github.com>
2024-06-26 16:21:33 +0200

    Implement BlockIdentificationScreen
    Signed-off-by: laura.voetzsch <laura.voetzsch@logiball.de>

--- app/src/main/java/com/example/computerarchitecture/ui/screens/caching/BlockIdentificationScreen.kt
+++ app/src/main/java/com/example/computerarchitecture/ui/screens/caching/BlockIdentificationScreen.kt
@@ -1,11 +1,14 @@
 package com.example.computerarchitecture.ui.screens.caching
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -47,9 +50,15 @@ fun BlockIdentificationScreen(modifier: Modifier = Modifier, navigateBack: () ->
         },
     ) { paddingValues ->
         Column(Modifier.padding(paddingValues)) {
-            DirectMapping(Modifier.padding(16.dp))
+            DirectMapping()
             SetAssociativeMapping()
             AssociativeMapping()
+            HorizontalDivider()
+            MemoryAddress(Modifier.padding(16.dp))
+            Tag(Modifier.padding(16.dp))
+            Index(Modifier.padding(16.dp))
+            Offset(Modifier.padding(16.dp))
+
         }
     }
 }
@@ -61,6 +70,49 @@ fun BlockIdentificationScreen(modifier: Modifier = Modifier, navigateBack: () ->
  */
 @Composable
 private fun DirectMapping(modifier: Modifier = Modifier) {
+    ListItem(
+        headlineContent = { Text(text = stringResource(R.string.direct_mapping)) },
+        modifier = modifier,
+        supportingContent = { Text(text = stringResource(R.string.direct_mapping_block_identification)) },
+    )
+
+}
+
+/**
+ * Displays a list item for set associative mapping.
+ *
+ * @param modifier The modifier for the layout
+ */
+@Composable
+private fun SetAssociativeMapping(modifier: Modifier = Modifier) {
+    ListItem(
+        headlineContent = { Text(text = stringResource(R.string.set_associative_mapping)) },
+        modifier = modifier,
+        supportingContent = { Text(text = stringResource(R.string.set_associative_mapping_block_identification)) },
+    )
+}
+
+/**
+ * Displays a list item for associative mapping.
+ *
+ * @param modifier The modifier for the layout
+ */
+@Composable
+private fun AssociativeMapping(modifier: Modifier = Modifier) {
+    ListItem(
+        headlineContent = { Text(text = stringResource(R.string.associative_mapping)) },
+        modifier = modifier,
+        supportingContent = { Text(text = stringResource(R.string.associative_mapping_block_identification)) },
+    )
+}
+
+/**
+ * Displays the structure of an address.
+ *
+ * @param modifier The modifier for the layout
+ */
+@Composable
+private fun MemoryAddress(modifier: Modifier = Modifier) {
     Row(
         modifier
             .fillMaxWidth()
@@ -89,21 +141,72 @@ private fun DirectMapping(modifier: Modifier = Modifier) {
 }
 
 /**
- * Displays a list item for set associative mapping.
+ * Displays a column for the tag.
  *
  * @param modifier The modifier for the layout
  */
 @Composable
-private fun SetAssociativeMapping(modifier: Modifier = Modifier) {
+private fun Tag(modifier: Modifier = Modifier) {
+    Column(modifier, Arrangement.spacedBy(8.dp)) {
+        Text(
+            text = stringResource(R.string.tag),
+            style = MaterialTheme.typography.titleMedium
+        )
+        Text(
+            text = stringResource(R.string.tag_length),
+            style = MaterialTheme.typography.labelMedium
+        )
+        Text(
+            text = stringResource(R.string.tag_description),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
 }
 
 /**
- * Displays a list item for associative mapping.
+ * Displays a column for the index.
  *
  * @param modifier The modifier for the layout
  */
 @Composable
-private fun AssociativeMapping(modifier: Modifier = Modifier) {
+private fun Index(modifier: Modifier = Modifier) {
+    Column(modifier, Arrangement.spacedBy(8.dp)) {
+        Text(
+            text = stringResource(R.string.index),
+            style = MaterialTheme.typography.titleMedium
+        )
+        Text(
+            text = stringResource(R.string.index_length),
+            style = MaterialTheme.typography.labelMedium
+        )
+        Text(
+            text = stringResource(R.string.index_description),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+/**
+ * Displays a column for the offset.
+ *
+ * @param modifier The modifier for the layout
+ */
+@Composable
+private fun Offset(modifier: Modifier = Modifier) {
+    Column(modifier, Arrangement.spacedBy(8.dp)) {
+        Text(
+            text = stringResource(R.string.offset),
+            style = MaterialTheme.typography.titleMedium
+        )
+        Text(
+            text = stringResource(R.string.offset_length),
+            style = MaterialTheme.typography.labelSmall
+        )
+        Text(
+            text = stringResource(R.string.offset_description),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
 }
 
 /**
