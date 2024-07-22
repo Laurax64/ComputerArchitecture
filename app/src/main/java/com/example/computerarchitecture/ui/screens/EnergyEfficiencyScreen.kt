4e0c662fd0896fd9fc19ebc99989d6bc05afed9f
laura.voetzsch <laura.voetzsch@logiball.de>
2024-07-22 12:13:20 +0200

    Implement EnergyEfficiencyScreen
    Signed-off-by: laura.voetzsch <laura.voetzsch@logiball.de>

--- app/src/main/java/com/example/computerarchitecture/ui/screens/EnergyEfficiencyScreen.kt
+++ app/src/main/java/com/example/computerarchitecture/ui/screens/EnergyEfficiencyScreen.kt
@@ -1,9 +1,17 @@
 package com.example.computerarchitecture.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
@@ -36,6 +44,7 @@ fun EnergyEfficiencyScreen(
             Modifier
                 .padding(it)
                 .padding(start = 16.dp, end = 16.dp)
+                .fillMaxWidth()
         )
     }
 }
@@ -47,6 +56,19 @@ fun EnergyEfficiencyScreen(
  */
 @Composable
 fun EnergyEfficiencyScreen(modifier: Modifier = Modifier) {
+    Card(modifier) {
+        Column(Modifier.padding(8.dp), Arrangement.spacedBy(8.dp)) {
+            Text(
+                text = stringResource(R.string.energy_formula),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Image(
+                painter = painterResource(id = R.drawable.energy_formula),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
 }
 
 /**
